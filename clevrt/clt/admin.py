c5690f0fddf3c6a9b6d4d7518ce4fa38566e803a
doroshenko-online <doroshenko@tutanota.com>
2020-07-02 23:45:35 +0300

    edit main page + add icons

--- clevrt/clt/admin.py
+++ clevrt/clt/admin.py
@@ -3,6 +3,8 @@ from .models import *
 
 # Register your models here.
 
+admin.site.register(City)
+admin.site.register(Country)
 admin.site.register(Client)
 admin.site.register(Ip_List)
 admin.site.register(Client_Gateway)
--- clevrt/clt/models.py
+++ clevrt/clt/models.py
@@ -3,6 +3,26 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Country(models.Model):
+    country = models.CharField(max_length=100, verbose_name='страна')
+
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страны'
+
+    def __str__(self):
+        return self.country
+
+class City(models.Model):
+    city = models.CharField(max_length=100, verbose_name='город')
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.city
+
 class Client(models.Model):
     ON = 'On'
     OFF = 'Off'
@@ -12,14 +32,20 @@ class Client(models.Model):
         (OFF, 'Отключенный'),
         (POT, 'Потенциальный'),
     ]
-    name = models.CharField(max_length=100, verbose_name='Название службы')
-    officeIp1 = models.CharField(max_length=16, blank=True, verbose_name='оффисный IP1')
-    officeIp2 = models.CharField(max_length=16, blank=True, verbose_name='оффисный IP2')
-    officeIp3 = models.CharField(max_length=16, blank=True, verbose_name='оффисный IP3')
-    active = models.CharField(max_length=3, choices=CLIENT_STATUS, default=POT, verbose_name='Активный клиент')
+    name = models.CharField(max_length=100, verbose_name='название службы')
+    country = models.ForeignKey(Country, max_length=100, blank=True, null=True, on_delete=models.DO_NOTHING, verbose_name='страна')
+    city = models.ForeignKey(City, max_length=100, blank=True, null=True, on_delete=models.DO_NOTHING, verbose_name='город')
+    hostname = models.CharField(max_length=100, blank=True, default='', verbose_name='хостнейм сервера')
+    title_comment = models.CharField(max_length=50, blank=True, default='', verbose_name='комментарий клиента(виден в списке клиентов)')
+    officeIp1 = models.CharField(max_length=16, blank=True, verbose_name='офисный IP1')
+    officeIp2 = models.CharField(max_length=16, blank=True, verbose_name='офисный IP2')
+    officeIp3 = models.CharField(max_length=16, blank=True, verbose_name='офисный IP3')
+    officeIp4 = models.CharField(max_length=16, blank=True, verbose_name='офисный IP4')
+    client_status = models.CharField(max_length=3, choices=CLIENT_STATUS, default=POT, verbose_name='статус клиента')
     hide = models.BooleanField(default=False, verbose_name='Скрыт')
-    additional_info = models.TextField(max_length=500, null=True, blank=True, verbose_name='Дополнительная информация')
-    connection_date = models.DateField(null=True, blank=True, verbose_name='Дата подключения')
+    additional_info = models.TextField(max_length=500, default='', blank=True, verbose_name='дополнительная информация')
+    connection_date = models.DateField(null=True, blank=True, verbose_name='дата подключения')
+    last_activity = models.DateTimeField(auto_now=True, verbose_name='последнее посещение')
 
     class Meta:
         verbose_name = 'Клиент'
@@ -71,7 +97,7 @@ class Client_Gateway(models.Model):
     port = models.CharField(max_length=6, blank=True, null=True, verbose_name='port')
     login = models.CharField(max_length=100, verbose_name='логин')
     secret = models.CharField(max_length=100, verbose_name='пароль')
-    type = models.CharField(max_length=50, choices=TYPES, default='Unknown', verbose_name='вендор оборудования')
+    type_gateway = models.CharField(max_length=50, choices=TYPES, default='Unknown', verbose_name='вендор оборудования')
 
     class Meta:
         verbose_name = 'Шлюз'
