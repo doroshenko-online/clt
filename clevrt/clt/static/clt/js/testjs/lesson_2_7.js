2c607a1cda2df219d706cb6f1fc2cea203bf66c3
doroshenko <doroshenko.tutanota.com>
2020-09-16 22:12:17 +0300

    lesson_2_9

---
+++ clevrt/clt/static/clt/js/testjs/lesson_2_7.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(0);
+console.log('');
+console.log(typeof('')); //String
+console.log(typeof(0)); //Number
+console.log(0 === ''); //false
+console.log(5 == '5'); //true
+console.log('AAB' == 'BBE'); //false
